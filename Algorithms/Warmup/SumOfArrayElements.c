c63f91737c8c98430405afda00404b3d8cf35457
Krishnakanth Yachareni <yacharenikrishnakanth@gmail.com>
2017-02-28 17:49:44 +0530

    Hacker
    rank

---
+++ Algorithms/Warmup/SumOfArrayElements.c
@@ -0,0 +1,44 @@
+/*Given an array of  integers, can you find the sum of its elements?
+
+Input Format
+
+The first line contains an integer, , denoting the size of the array. 
+The second line contains  space-separated integers representing the array's elements.
+
+Output Format
+
+Print the sum of the array's elements as a single integer.
+
+Sample Input
+
+6
+1 2 3 4 10 11
+Sample Output
+
+31
+Explanation
+
+We print the sum of the array's elements, which is: . */
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n,i,sum=0; 
+    scanf("%d",&n);
+    int arr[n];
+    for(int arr_i = 0; arr_i < n; arr_i++){
+       scanf("%d",&arr[arr_i]);
+    }
+    while(i<n)
+        {
+        sum=sum+arr[i];
+        i++;
+    }
+    printf("%d",sum);
+    return 0;
+}
